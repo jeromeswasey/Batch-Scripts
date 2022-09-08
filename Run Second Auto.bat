fb24d9b05a24c22b6f690ee7e948ea8eed72f074
jeromeswasey <105822554+jeromeswasey@users.noreply.github.com>
2022-09-08 14:16:03 -0400

    Update Run Second Auto.bat

--- Run Second Auto.bat
+++ Run Second Auto.bat
@@ -1,1 +1,2 @@
+################# A batch script to execute a powershell file used in a scheduled PS script to call windows task scheduler using the batch script below
 Powershell.exe -executionpolicy remotesigned -File  "C:\Temp\Powershell Scripts\Second Automation Script.ps1"
